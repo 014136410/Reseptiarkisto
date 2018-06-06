e6fd489e44623e4315f5b9e5653d2ab7cf867124
Julia Palorinne <julia.palorinne@helsinki.fi>
2018-06-07 01:06:25 +0300

    v5

--- src/main/java/tikape/huonekalut/Main.java
+++ src/main/java/tikape/huonekalut/Main.java
@@ -86,5 +86,15 @@ public class Main {
             return "";
         });
     }
+    
+    public static Connection getConnection() throws Exception {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        if (dbUrl != null && dbUrl.length() > 0) {
+            return DriverManager.getConnection(dbUrl);
+        }
+
+        return DriverManager.getConnection("jdbc:sqlite:huonekalut.db");
+    }
+    
 
 }
